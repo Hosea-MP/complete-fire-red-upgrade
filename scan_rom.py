4d6e9a6b95bf01b7504492642e19c874016d7fc9
hoseamp <76258414+Hosea-MP@users.noreply.github.com>
2025-08-05 22:10:30 -0500

    Write table pointers to files

--- scan_rom.py
+++ scan_rom.py
@@ -14,13 +14,20 @@ def scan(path):
         'trainer_class_names': 0x811B4B4,
         'trainers': 0x800FC00
     }
+    results = {}
     for name, addr in addresses.items():
         ptr = read_ptr(data, addr)
         if 0x08000000 <= ptr < 0x08000000 + len(data):
             offset = ptr - 0x08000000
-            print(name, f'{ptr:#010x}', f'{offset:#010x}')
+            results[name] = f"{name} {ptr:#010x} {offset:#010x}\n"
         else:
-            print(name, 'invalid')
+            results[name] = f"{name} invalid\n"
+    with open('pokemon_tables.txt', 'w') as f:
+        f.write(results['species_names'])
+        f.write(results['base_stats'])
+    with open('trainer_tables.txt', 'w') as f:
+        f.write(results['trainer_class_names'])
+        f.write(results['trainers'])
 
 if __name__ == '__main__':
     scan(sys.argv[1] if len(sys.argv) > 1 else 'rr.gba')
